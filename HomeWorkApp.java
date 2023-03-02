f41099b25fca680a5952aebda79df665d91818bc
Nikita Vasilev <nikitavasilev025@gmail.com>
2023-03-02 18:52:26 +0300

    Lesson1

---
+++ HomeWorkApp.java
@@ -0,0 +1,53 @@
+package lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    private static void checkSumSign() {
+        int a = 100;
+        int b = -500;
+
+        if ( a + b >=0 ) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+    public static void printColor() {
+        int value = 100;
+
+        if ( value <= 0) {
+            System.out.println("Красный");
+        } else if ( value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+
+    }
+
+    public static void compareNumbers() {
+        int a = 3;
+        int b = 3;
+
+        if ( a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+}
